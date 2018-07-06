6e0301891264e14d450df9ba04cc7d6b7b884f1e
Justin Dang <dangjustintime@gmail.com>
2018-07-06 11:27:46 -0400

    added heroku port

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("waddup");
+// dependencies
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (request, response) => {
+    response.send("hello");
+});
+
+// listening to port
+app.listen(PORT, () => {
+    console.log("listening to port", PORT);
+});
+
