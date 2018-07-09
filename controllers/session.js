d993e13de0d49bce7aac61284a97e36a26601d1b
Justin Dang <dangjustintime@gmail.com>
2018-07-09 00:41:27 -0400

    added sign in and sign out route

--- controllers/session.js
+++ controllers/session.js
@@ -2,3 +2,24 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const bcrypt = require("bcrypt");
+
+// sign in route
+router.post("/", (request, response) => {
+    User.findOne({ email: request.body.email }, (error, foundUser) => {
+        if (request.body.password == foundUser.password) {
+            request.session.currentUser = foundUser;
+            response.redirect("/");
+        } else {
+            response.send("wrong password");
+        }
+    });
+});
+
+// sign out route
+router.delete("/", (request, response) => {
+    request.session.destroy(() => {
+        response.redirect("/");
+    })
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // dependencies
 const express = require("express");
+const session = require("express-session");
 const methodOverride = require("method-override");
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,16 +8,26 @@ const mongoose = require("mongoose");
 const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
 const User = require("./models/user.js");
 const userController = require("./controllers/user.js");
+const sessionController = require("./controllers/session.js");
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(session({
+    secret: "maytheforcebewithyou",
+    resave: false,
+    saveUninitialized: false
+}));
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
 app.use("/user/", userController);
+app.use("/session/", sessionController);
 
-// test route
+// home route
 app.get("/", (request, response) => {
-    response.send("chee chee boo boo");
+    response.render("home.ejs", {
+        currentUser: request.session.currentUser
+    });
 });
 
 // listening to port
